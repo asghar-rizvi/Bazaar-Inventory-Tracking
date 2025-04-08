198c563bc8fd43680155e87ce36050191277d4cd
Asghar Qamber Rizvi <asgharqamberrozvi@gmail.com>
2025-04-08 16:25:02 +0500

    Final Commit

--- Stage 2/init_db.py
+++ Stage 2/init_db.py
@@ -1,15 +1,9 @@
 from app import app, db
-from model import Store, ProductCatalog, StoreInventory, User
 
 def initialize_database():
     with app.app_context():
         db.create_all()
         print("Database tables created successfully")
 
-def add_user_table():
-    with app.app_context():
-        User.__table__.create(db.engine)
-
 if __name__ == '__main__':
-    # initialize_database()
-    add_user_table()
+    initialize_database()
